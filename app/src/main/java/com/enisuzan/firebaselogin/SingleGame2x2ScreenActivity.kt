344c24d1951f47ebe06bb8477799841bcef4ff4a
uzanenis <uzanenis@outlook.com>
2022-12-11 15:31:39 +0300

    score and timer add

--- app/src/main/java/com/enisuzan/firebaselogin/SingleGame2x2ScreenActivity.kt
+++ app/src/main/java/com/enisuzan/firebaselogin/SingleGame2x2ScreenActivity.kt
@@ -3,6 +3,7 @@ package com.enisuzan.firebaselogin
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Base64
 import android.util.Log
 import android.widget.ImageButton
@@ -42,18 +43,20 @@ class SingleGame2x2ScreenActivity : AppCompatActivity() {
             "14",
             "") */
 
-
-
-
-
-
-
-
         val cardListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 var items: ArrayList<Card> = arrayListOf()
                 var items2: ArrayList<Card> = arrayListOf()
                 var currentCard: Int? = null
+                var currentTime: Long? = null
+                var playerScore: Double? = 0.0
+                var houseScores: HashMap<String, Double> = hashMapOf(
+                    "Slytherin" to 2.0,
+                    "Gryffindor" to 2.0,
+                    "Ravenclaw" to 1.0,
+                    "Hufflepuff" to 1.0
+                )
+
                 println("Firebase ile veriler yüklendi!!")
 
                 for (postSnapshot in snapshot.children) {
@@ -72,35 +75,91 @@ class SingleGame2x2ScreenActivity : AppCompatActivity() {
                     screenItems.add(items[item].copy())
                 }
                 screenItems.shuffle()
+                //Sayaç
+                val timer = object : CountDownTimer(45000, 1000) {
+                    override fun onTick(p0: Long) {
+                        val seconds = p0 / 1000
+                        currentTime = seconds
+                        binding.timeCounter.text = "00:$seconds"
+                    }
 
-                fun resetUi(){
-                    for(card in screenItems){
-                        if(!card.isMatched!!){
+                    override fun onFinish() {
+                        Toast.makeText(
+                            this@SingleGame2x2ScreenActivity,
+                            "Süre Bitti!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                }
+                timer.start()
+
+                //Kartları resetleme
+                fun resetUi() {
+                    for (card in screenItems) {
+                        if (!card.isMatched!!) {
                             card.isFaceUp = false
                         }
                     }
                 }
 
-                fun checkMatch(card1: Int, card2: Int){
-                    if(screenItems[card1].name == screenItems[card2].name){
-                        Toast.makeText(this@SingleGame2x2ScreenActivity,"Cards matched! Good job!", Toast.LENGTH_SHORT).show()
+                //Eşleşme kontrolü
+                fun checkMatch(card1: Int, card2: Int) {
+                    fun String.trimAndDouble() = trim().toDouble()
+                    if (screenItems[card1].name == screenItems[card2].name) {
+                        val houseScore: Double = if (screenItems[card1].house.toString() == "Gryffindor"
+                            || screenItems[card1].house.toString() == "Slytherin") 2.0 else 1.0;
+                        Toast.makeText(
+                            this@SingleGame2x2ScreenActivity,
+                            "Cards matched! Good job!",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         screenItems[card1].isMatched = true
                         screenItems[card2].isMatched = true
+                        playerScore = playerScore?.plus((2.0 * (screenItems[card1].score?.trimAndDouble()!!) * houseScore) * (currentTime?.toDouble()!! / 10))
+                        binding.playerOnePoints.text = playerScore.toString()
+                    } else {
+                        if(screenItems[card1].house == screenItems[card2].house){
+                            val houseScore: Double = if (screenItems[card1].house.toString() == "Gryffindor"
+                                || screenItems[card1].house.toString() == "Slytherin") 2.0 else 1.0;
+                            playerScore = playerScore?.minus(((screenItems[card1].score?.trimAndDouble()!! +
+                                    screenItems[card2].score?.trimAndDouble()!!) / houseScore) *
+                                    (45.0 - currentTime?.toDouble()!! / 10)
+                            )
+                            binding.playerOnePoints.text = playerScore.toString()
+
+                        } else {
+                            val houseScore1: Double? = if (screenItems[card1].house.toString() == "Gryffindor"
+                                || screenItems[card1].house.toString() == "Slytherin") 2.0 else 1.0;
+                            val houseScore2: Double? = if (screenItems[card2].house.toString() == "Gryffindor"
+                                || screenItems[card1].house.toString() == "Slytherin") 2.0 else 1.0;
+                            playerScore = playerScore?.minus(((screenItems[card1].score?.trimAndDouble()!! +
+                                    screenItems[card2].score?.trimAndDouble()!!) / 2) * (houseScore1!! * houseScore2!!) *
+                                    (45.0 - currentTime?.toDouble()!! / 10)
+                            )
+                            binding.playerOnePoints.text = playerScore.toString()
+
+                        }
                     }
                 }
 
-                fun updateCards(position: Int){
+                //Cardların durumlarını kontrol etme
+                fun updateCards(position: Int) {
                     val card = screenItems[position]
-                    println(card.name)
-                    if(card.isFaceUp == true){
-                        Toast.makeText(this@SingleGame2x2ScreenActivity,"Hatalı oynama!", Toast.LENGTH_SHORT).show()
+                    println("${card.name} -> ${card.house}")
+                    if (card.isFaceUp == true) {
+                        Toast.makeText(
+                            this@SingleGame2x2ScreenActivity,
+                            "Hatalı oynama!",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         return
                     }
 
-                    if(currentCard == null){
+                    if (currentCard == null) {
                         currentCard = position
                         resetUi()
-                    }else{
+                    } else {
                         checkMatch(currentCard!!, position)
                         currentCard = null
                     }
@@ -108,18 +167,20 @@ class SingleGame2x2ScreenActivity : AppCompatActivity() {
                     card.isFaceUp = !card.isFaceUp!!
 
                 }
-                fun updateUi(){
+
+                //Cardları güncelleme
+                fun updateUi() {
                     screenItems.forEachIndexed { index, card ->
                         val button = buttons[index]
-                        if(card.isMatched == true){
+                        if (card.isMatched == true) {
                             button.alpha = 0.3f
                         }
-                        if(card.isFaceUp!!){
+                        if (card.isFaceUp!!) {
                             val imageBytes = Base64.decode(card.image, Base64.DEFAULT)
                             val decodedImage =
                                 BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
                             button.setImageBitmap(decodedImage)
-                        }else {
+                        } else {
                             button.setImageResource(R.drawable.cardfront)
                         }
                     }
@@ -141,7 +202,7 @@ class SingleGame2x2ScreenActivity : AppCompatActivity() {
         database.addValueEventListener(cardListener)
 
 
-        val difficulty = intent.getStringExtra("Difficulty")
+        //val difficulty = intent.getStringExtra("Difficulty")
 
 
     }
